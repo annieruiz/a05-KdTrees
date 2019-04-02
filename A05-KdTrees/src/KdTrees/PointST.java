15181a0b9788b44b2be62eae1934cefde6f58fb3
Aarceo1 <aarceo1@bruinmail.slcc.edu>
2019-04-02 15:30:31 -0600

    Update pointST

--- A05-KdTrees/src/KdTrees/PointST.java
+++ A05-KdTrees/src/KdTrees/PointST.java
@@ -1,96 +1,126 @@
-package KdTrees;
-
+package kdTree;
+ // GIT REPO
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.RedBlackBST;
 
 public class PointST<Value> {
 	int size;
-	RedBlackBST<Point2D, String> point;
-	
+	private RedBlackBST<Point2D, Value> st;
+
 	/**
-	 * construct an empty symbol table of points
+	 * Construct an empty symbol table of points
 	 */
 	public PointST() {
-		RedBlackBST<Point2D, String> point = new RedBlackBST<Point2D, String>();
-		//this.point = point;
+		st = new RedBlackBST<>();
 	}
-	
+
 	/**
 	 * is the symbol table empty?
-	 * @return true if empty, false if not
+	 * 
+	 * @return boolean true if empty, false if otherwise
 	 */
 	public boolean isEmpty() {
+//		return size() == 0;
+		return st.isEmpty();
 	}
-	
+
 	/**
-	 * number of points 
-	 * @return
+	 * number of points
+	 * 
+	 * @return int number of points
 	 */
 	public int size() {
-		if (size == 0) {
-			
-		}
+		return st.size();
 	}
-	
+
 	/**
-	 * associate the value val with point p
+	 * associate the value val with the point p
+	 * 
 	 * @param p
+	 *            Point2d key for the value
 	 * @param val
+	 *            value
 	 */
 	public void put(Point2D p, Value val) {
-		point.put(p, val);
+		if (p == null) //FIXME can val be null?
+			throw new NullPointerException("called put() with null point");
+		st.put(p, val);
 	}
-	
+
 	/**
-	 * value associated with point p 
+	 * value associated with point p
+	 * 
 	 * @param p
-	 * @return
+	 *            Point2d key to get
+	 * @return value at point p
 	 */
 	public Value get(Point2D p) {
-		
+		if (p == null)
+			throw new NullPointerException("called get() with null point");
+		return st.get(p);
 	}
-	
+
 	/**
-	 * does the symbol table contain point p? 
+	 * does the symbol table contain point p?
+	 * 
 	 * @param p
-	 * @return
+	 *            Point2d key to get
+	 * @return true if it contains point p, false if otherwise
 	 */
 	public boolean contains(Point2D p) {
-		
+		if (p == null)
+			throw new NullPointerException("called contains() with null point");
+		return st.contains(p);
 	}
-	
+
 	/**
-	 * all points in the symbol table 
-	 * @return
+	 * all the point in the symbol table
+	 * 
+	 * @return all the points in the symbol table
 	 */
-	public Iterable<Point2D> point() {
-		
-	}
-	
+	public Iterable<Point2D> points() {
+		return st.keys();
+	} 
+
 	/**
-	 * all points that are inside the rectangle
-	 * @param rect
-	 * @return
+	 * all the points that are inside the rectangle
+	 * @param rect rectangle we are searching inside
+	 * @return iterable containing all the points inside the rectangle
 	 */
 	public Iterable<Point2D> range(RectHV rect) {
-		
+		if (rect == null) throw new NullPointerException("called range() with null rectangle");
+		Queue<Point2D> que = new Queue<>();
+		for (Point2D p : st.keys()) {
+			if (rect.contains(p))
+				que.enqueue(p);
+		}
+		return que;
 	}
 	
 	/**
-	 * a nearest neighbor to point p; null if the symbol table is empty 
+	 * nearest neighbor to point p; null if the symbol table is empty
 	 * @param p
 	 * @return
 	 */
-	public Point2D nearest (Point2D p) {
-		
+	public Point2D nearest(Point2D p) { //Verify that we are going in the direction like if we were inserting the point into the graph.
+		if (p == null) throw new NullPointerException("called nearest() with null point");
+		Point2D nearest = st.max();
+		for (Point2D point : st.keys()) {
+			if (p.distanceSquaredTo(point) < p.distanceSquaredTo(nearest)) {
+				nearest = point;
+			}
+		}
+		return nearest;
 	}
-	
+
 	/**
-	 * unit testing of the methods (not graded) 
+	 * unit testing of the methods (not graded)
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 	}
 }
